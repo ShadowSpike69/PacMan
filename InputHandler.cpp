c633987a5090ab2dda5ea3f9eca56f424a6de159
ShadowSpike <vip.nimini@mail.ru>
2025-09-18 22:19:19 +0300

    Control Taste

---
+++ InputHandler.cpp
@@ -0,0 +1,9 @@
+﻿#include "InputHandler.hpp"
+#include <iostream>
+
+char InputHandler::getInput() {
+    char input;
+    std::cout << "Direcție (w/a/s/d): ";
+    std::cin >> input;
+    return input;
+}
